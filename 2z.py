2adea037212cf951347df981056baeeb8be3bab8
niyazful <56266317+niyazful@users.noreply.github.com>
2019-10-08 11:59:44 +0500

    Update 2z.py

--- 2z.py
+++ 2z.py
@@ -3,7 +3,5 @@ import json
 mayfile = "task_2.json"
 with open(mayfile,encoding="utf-8") as read_file:
     data = json.load(read_file)
-    m
-    y
     data.sort(key=lambda user: user["shop"])
     print (data)
